8865be4b3f42b21758a1b5b7eabcf87f58082e51
alexjr2001 <alexander.gomez@ucsp.edu.pe>
2025-10-01 05:58:50 -0500

    Habilitar CORS en backend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 // app.js (o index.js)
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
